bbea76e4b3479fb9a1e7339a8b02f5debc7acb33
Ricardo Zamudio <ricardo.zamudio.c@gmail.com>
2020-05-15 20:26:26 -0500

    CATALOGO DE WEBIARS PARTE 1

---
+++ fuente/public/js/archivo.js
@@ -0,0 +1,8 @@
+function mostrar() {
+    var x = document.getElementById("lista");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
